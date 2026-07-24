c102a07cf21c362c40b5a9cf773139c242d7ea04
Sonu Kumar <sonukumar03xt@gmail.com>
2026-07-25 02:05:51 +0530

    feat: add Treesitter text object mappings

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -3,8 +3,9 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
+		config = function()
+			local treesitter = require("nvim-treesitter")
+			local languages = {
 				"lua",
 				"vim",
 				"vimdoc",
@@ -19,18 +20,20 @@ return {
 				"json",
 				"html",
 				"css",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				disable = { "latex", "tex" }, -- VimTeX handles this better
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
-		},
+			}
+
+			treesitter.setup()
+			treesitter.install(languages)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local ok = pcall(vim.treesitter.start, args.buf)
+					if ok then
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
@@ -44,5 +47,43 @@ return {
 			trim_scope = "outer",
 			mode = "cursor",
 		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		init = function()
+			-- Avoid conflicts with Neovim's built-in filetype textobject mappings.
+			vim.g.no_plugin_maps = true
+		end,
+		config = function()
+			local textobjects = require("nvim-treesitter-textobjects")
+
+			textobjects.setup({
+				select = {
+					lookahead = true,
+					selection_modes = {
+						["@function.outer"] = "V",
+						["@function.inner"] = "V",
+						["@class.outer"] = "V",
+						["@class.inner"] = "V",
+					},
+				},
+			})
+
+			local select = require("nvim-treesitter-textobjects.select").select_textobject
+			vim.keymap.set({ "x", "o" }, "af", function()
+				select("@function.outer", "textobjects")
+			end, { desc = "Treesitter: Select function" })
+			vim.keymap.set({ "x", "o" }, "if", function()
+				select("@function.inner", "textobjects")
+			end, { desc = "Treesitter: Select inside function" })
+			vim.keymap.set({ "x", "o" }, "ac", function()
+				select("@class.outer", "textobjects")
+			end, { desc = "Treesitter: Select class" })
+			vim.keymap.set({ "x", "o" }, "ic", function()
+				select("@class.inner", "textobjects")
+			end, { desc = "Treesitter: Select inside class" })
+		end,
 	},
 }
